32af0592a1465639f48c7d2cc90caf662f8bf9c0
oscar sanchez <00oscarsanchez00@gmail.com>
2025-11-20 10:47:00 -0600

    added custom domain and got admin panel to admit user

--- week8/1-admin-ui/app.py
+++ week8/1-admin-ui/app.py
@@ -14,6 +14,8 @@ _ = Twin(app, "Twin",
          kb_id=kb.kb.knowledge_base_id,
          kb_data_src_id=kb.kb.data_source_id,
          kb_input_bucket=kb.input_bucket,
+         custom_domain_name="00oscar00.com",
+         custom_certificate_arn="arn:aws:acm:us-east-1:078109852507:certificate/25de5f2e-5c03-4cfa-95f6-1b29c99cf671",
          env=env,
         )
 
